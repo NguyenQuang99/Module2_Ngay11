ff52868fcc24c248700837098ab43a18a71d53b2
NguyenQuang99 <quanghero7@gmail.com>
2020-09-25 11:08:54 +0700

    Optional_TH_Queue/

---
+++ Optional_TH_Queue/MyQueue.java
@@ -0,0 +1,55 @@
+package Optional_TH_Queue;
+
+public class MyQueue {
+    private int capacity;
+    private int queueArr[];
+    private int head = 0;
+    private int tail = -1;
+    private int currentSize = 0;
+    public MyQueue(int queueSize) {
+        this.capacity = queueSize;
+        queueArr = new int[this.capacity];
+    }
+    public boolean isQueueFull() {
+        boolean status = false;
+        if(currentSize == capacity) {
+            status = true;
+        }
+        return false;
+    }
+    public boolean isQueueEmpty() {
+        boolean status = false;
+        if(currentSize == 0) {
+            status = true;
+        }
+        return  status;
+    }
+    public void enqueue(int item) {
+        if(isQueueFull()) {
+            System.out.println("Unable to add element" + item);
+        } else {
+            tail++;
+            if(tail == capacity -1) {
+                tail = 0;
+            }
+            queueArr[tail] = item;
+            currentSize ++;
+            System.out.println("Element" + item + "is pushed");
+        }
+    }
+    public void dequeue() {
+        if(isQueueEmpty()) {
+            System.out.println("Unable to remove element from Queue");
+        } else {
+            head++;
+            if(head == capacity -1) {
+                System.out.println("Pop  /// removed" + queueArr[head -1]);
+                head = 0;
+            }else {
+                System.out.println("removed" + queueArr[head -1]);
+            }
+            currentSize--;
+        }
+    }
+
+}
